a19dddabbb233c229d107e2a780f4fd17cb9e7ba
Artem Zhelonkin <tyo3436@gmail.com>
2020-11-19 01:07:12 +0300

    feature(daemon): #3 Add simple daemonization for server

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 """
 Данный модуль будет основной точкой входа
 """
+import os
+import sys
 import server
+from daemon import Daemon
+from loguru import logger
+
+logger.add("./log/daemon/debug.log", format="{time} {level} {message}",
+           level="DEBUG",
+           rotation="10MB")
+logger = logger.opt(colors=True)
 
 SERVER_ADDR: str = 'localhost'
 SERVER_PORT: int = 9000
@@ -10,16 +19,34 @@ LISTEN_CLIENTS_NUMB: int = 12
 MAX_SERVER_BUFFER_SIZE: int = 4096
 
 
-def sample_sum_func(first_number, second_number) -> int:
-    """
-    Sample function: get two arguments
-    and return their sum
-    """
-    print('Find sum of %s and %s' % (first_number, second_number))
-    return first_number + second_number
+class App(Daemon):
+    def run(self):
+        server.run_server(SERVER_ADDR, SERVER_PORT, LISTEN_CLIENTS_NUMB,
+                          MAX_SERVER_BUFFER_SIZE)
 
 
-# Input point if we directly run this script
+pid_file = './daemon_pid.pid'
+stdin = os.devnull
+stdout = './log/output.log'
+stderr = './log/error.log'
+home_dir = '.'
+
+daemon_main = App(pid_file, stdin, stdout, stderr, home_dir)
+
+control = sys.argv[1]
+
+if control == 'start':
+    daemon_main.start()
+elif control == 'stop':
+    daemon_main.stop()
+elif control == 'restart':
+    daemon_main.restart()
+elif control == 'run':
+    daemon_main.run()
+elif control == 'get_pid':
+    logger.info("Daemon PID: {}", daemon_main.get_pid())
+else:
+    daemon_main.is_running()
+
 if __name__ == '__main__':
-    server.run_server(SERVER_ADDR, SERVER_PORT, LISTEN_CLIENTS_NUMB,
-                      MAX_SERVER_BUFFER_SIZE)
+    pass
